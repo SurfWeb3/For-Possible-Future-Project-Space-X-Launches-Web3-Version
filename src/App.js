cb131bafc3ec935df759f0419df3cefcb1000830
unknown <aarkramer@hotmail.com>
2023-10-07 14:00:22 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import SpaceFlight from "./SpaceFlight"
+//, { useState, useEffect } from "react"
+//import axios from "axios";
+
+function App() {
+  return(
+    <div>
+      <h1>SpaceX Flights</h1>
+      <SpaceFlight />
+    </div>
+  )
+}
+
+export default App;
